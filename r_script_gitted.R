49cc676614350c59ac857aebf67192cd0436c282
thierrymonthe <thierry.monthe@gmx.de>
2019-01-08 19:28:12 +0100

    first commit from RStudio

---
+++ r_script_gitted.R
@@ -0,0 +1,16 @@
+# new script on git
+
+x <- 1
+print(x)
+
+
+# We have to configure RStudio to recognize the email address of the git account in order to commit and push
+# Run
+# 
+# git config --global user.email "you@example.com"
+# git config --global user.name "Your Name"
+# 
+# to set your account's default identity.
+# Omit --global to set the identity only in this repository.
+# 
+# fatal: unable to auto-detect email address (got 'Thierry Monthe@DESKTOP-HSFRIFR.(none)')
